6c03ed3f9a1388fa61cf2e77c6782a6a903a8c37
Hoàng Nguyễn <hoangnvse184515@fpt.edu.vn>
2024-10-21 15:58:32 +0700

    fix3

--- KoiShop.Application/Service/OrderService.cs
+++ KoiShop.Application/Service/OrderService.cs
@@ -43,6 +43,7 @@ namespace KoiShop.Application.Service
             var oddto = _mapper.Map<IEnumerable<OrderDetailDtos>>(od);
             return oddto;
         }
+        // abc
         public async Task<IEnumerable<OrderDtos>> GetOrder()
         {
             if (_userContext.GetCurrentUser() == null || _userStore == null)
